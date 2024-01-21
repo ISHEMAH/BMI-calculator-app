7fadee03c71beef8437ff3922616b942da287656
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2024-01-21 23:03:25 +0200

    layout phase 2

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0XFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -18,58 +25,89 @@ class _InputPageState extends State<InputPage> {
                 child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0XFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      )),
+                  child: ReusableCard(
+                    color: activeCardColor,
+                    cardChild: iconContent(text: 'MALE',icon: FontAwesomeIcons.mars,),
+                  ),
                 ),
                 Expanded(
-                  child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0XFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      )),
-                ),
+                    child: ReusableCard(
+                        color: activeCardColor,
+                        cardChild: iconContent(text: 'FEMALE',icon: FontAwesomeIcons.venus,),)),
               ],
             )),
             Expanded(
               child: Expanded(
-                child: Container(
-                    child: Text('Ishema'),
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0XFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    )),
+                child: ReusableCard(
+                    color: activeCardColor,
+                    cardChild: Column(children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'MALE',
+                        style:
+                            TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98)),
+                      )
+                    ])),
               ),
             ),
             Expanded(
                 child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                      child: Text('Ishema'),
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0XFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      )),
+                  child: ReusableCard(
+                      color: activeCardColor,
+                      cardChild: Column(children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(
+                              fontSize: 18.0, color: Color(0xFF8D8E98)),
+                        )
+                      ])),
                 ),
                 Expanded(
-                  child: Container(
-                      child: Text('Ishema'),
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0XFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      )),
+                  child: ReusableCard(
+                      color: activeCardColor,
+                      cardChild: Column(children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(
+                              fontSize: 18.0, color: Color(0xFF8D8E98)),
+                        )
+                      ])),
                 ),
               ],
-            ))
+            )),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
           ],
         ));
   }
 }
+
+
+
+
